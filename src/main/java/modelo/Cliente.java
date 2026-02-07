cca4bfb7a2ea9bd630ace17ab2389c17fd6b5795
Pinto <pinto702rodri@gmail.com>
2026-02-07 17:09:54 -0500

    🧬:boom: feat: add Persona base class and extend Cliente for inheritance

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -1,58 +1,21 @@
 
 package modelo;
 
-public class Cliente {
-    private int id;
-    private String nombre, identificacion, correo, telefono;
+public class Cliente extends Persona{
 
     public Cliente(int id, String nombre, String identificacion, String correo, String telefono) {
-        this.id = id;
-        this.nombre = nombre;
-        this.identificacion = identificacion;
-        this.correo = correo;
-        this.telefono = telefono;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getIdentificacion() {
-        return identificacion;
-    }
-
-    public void setIdentificacion(String identificacion) {
-        this.identificacion = identificacion;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+        super(id, nombre, identificacion, correo, telefono);
     }
     
-    
-    
+    @Override
+    public String toString() {
+        return """
+               ID: %d
+               Nombre: %s
+               Identificacion: %s
+               Correo: %s
+               Telefono: %s
+               """.formatted(getId(), getNombre(), getIdentificacion(), getCorreo(), getTelefono());
+    }
+
 }
