6a1e7b97a4a4fab1bd0dafc538daac195ab184dc
Pinto <pinto702rodri@gmail.com>
2026-02-11 17:19:55 -0500

    🛠️ feat: initial setup of ArchivoUtils for file handling logic

--- src/main/java/controlador/util/ArchivoUtils.java
+++ src/main/java/controlador/util/ArchivoUtils.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlador.util;
 
-/**
- *
- * @author camper
- */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import modelo.Venta;
+
 public class ArchivoUtils {
-    
-}
+
+    private ArrayList<Venta> ventas = new ArrayList<>();
+    private final String ARCHIVO = "ventas.dat";
+
+    public void cargar() {
+        File f = new File(ARCHIVO);
+        if (!f.exists()) {
+            return;
+        }
+        try (ObjectOutputStream ois = new ObjectOutputStream(new FileOutputStream(ARCHIVO))) {
+            ventas = (ArrayList<Venta>) ois.replaceObject();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void exportarBackup() {
+        JFileChooser j = new JFileChooser();
+        j.setDialogTitle("Escoja la ruta a guardar");
+        int op = j.showSaveDialog(j);
+        if (op == JFileChooser.APPROVE_OPTION) {
+            File destino = j.getSelectedFile();
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ARCHIVO))) {
+                oos.writeObject(ventas);
+                System.out.println("BASE DE DATOS EXPORTADA CORRECTAMENTE");
+            } catch (Exception e) {
+            }
+        }
+
+    }
